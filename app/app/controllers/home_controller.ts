149b1de059793fd240a97822de3d7a33aab9e8fc
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2026-01-17 02:18:18 +0100

    Remove debug code from home

--- app/app/controllers/home_controller.ts
+++ app/app/controllers/home_controller.ts
@@ -1,13 +1,7 @@
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class HomeController {
-  async index({ inertia, logger, oauth }: HttpContext) {
-    logger.debug(oauth)
-    let token
-    if (oauth) {
-      token = await oauth.getTokenInfo()
-      logger.debug({ token })
-    }
-    return inertia.render('home', { debug: JSON.stringify(token, null, 2) })
+  async index({ inertia }: HttpContext) {
+    return inertia.render('home')
   }
 }
