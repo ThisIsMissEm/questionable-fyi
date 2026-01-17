acac5afca8d1c274e9f952ef4205a335db099015
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2026-01-17 02:25:41 +0100

    Fix models for oauth sessions and state

--- app/app/models/oauth_session.ts
+++ app/app/models/oauth_session.ts
@@ -1,20 +1,12 @@
 import { BaseModel, column } from '@adonisjs/lucid/orm'
-import { safeParseJson } from '../utils/json.js'
 import { DateTime } from 'luxon'
 
 export default class OauthSession extends BaseModel {
   @column({ isPrimary: true })
   declare sub: string
 
-  @column({
-    consume: (value) => {
-      return safeParseJson(value)
-    },
-    prepare: (value) => {
-      return JSON.stringify(value)
-    },
-  })
-  declare value: any
+  @column()
+  declare value: string
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime
