660ca981b90ed2550768674d926ccb0816cfdf88
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2026-01-24 22:30:44 +0100

    Upgrade to @thisismissem/adonisjs-atproto-oauth v1.0.0 (#8)

---
+++ .changeset/silver-words-brake.md
@@ -0,0 +1,5 @@
+---
+"app": patch
+---
+
+Upgrade to @thisismissem/adonisjs-atproto-oauth v1.0.0
--- app/app/validators/onboarding.ts
+++ app/app/validators/onboarding.ts
@@ -1,7 +1,5 @@
 import vine from '@vinejs/vine'
 
-export const storeProfileValidator = vine.compile(
-  vine.object({
-    displayName: vine.string().minLength(1).maxLength(640),
-  })
-)
+export const storeProfileValidator = vine.create({
+  displayName: vine.string().minLength(1).maxLength(640),
+})
