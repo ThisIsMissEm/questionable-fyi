47b5e935f352411da24b92c046bf3f4e48d5b2ae
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2026-01-24 22:10:18 +0100

    Upgrade to LexIndexer with @thisismissem/adonisjs-atproto-tap v2.0.0 (#7)

--- app/start/indexer.ts
+++ app/start/indexer.ts
@@ -1,63 +1,48 @@
-import indexer from '@thisismissem/adonisjs-atproto-tap/services/indexer'
-import tap from '@thisismissem/adonisjs-atproto-tap/services/tap'
+import app from '@adonisjs/core/services/app'
 import logger from '@adonisjs/core/services/logger'
+import tap from '@thisismissem/adonisjs-atproto-tap/services/tap'
+import { LexIndexer } from '@atproto/tap'
 import { DateTime } from 'luxon'
 
 import * as lexicon from '#lexicons/index'
 import Account from '#models/account'
 import Profile from '#models/profile'
-import { RecordEvent } from '@atproto/tap'
+
+const indexer = new LexIndexer()
 
 indexer.identity(async (evt) => {
+  logger.debug(evt, `Updating account for ${evt.did}`)
   await Account.upsert(evt)
 })
 
-indexer.record(async (evt: RecordEvent) => {
-  const uri = `at://${evt.did}/${evt.collection}/${evt.rkey}`
-  if (evt.action === 'create' || evt.action === 'update') {
-    logger.info(
-      { event: evt.action, uri, cid: evt.cid, rev: evt.rev },
-      `Tap: ${evt.action}: ${uri}`
-    )
-  } else {
-    logger.info({ event: evt.action, uri }, `Tap: deleted: ${uri}`)
+indexer.delete(lexicon.fyi.questionable.actor.profile, async (evt) => {
+  if (evt.rkey !== 'self') {
+    return
   }
 
-  const account = await Account.find(evt.did)
-  if (!account) {
-    // FIXME: We probably need to call resolveIdentity here:
-    const repo = await tap.getRepoInfo(evt.did)
+  logger.debug(`Deleting profile for ${evt.did}`)
 
-    await Account.upsert({
-      did: repo.did,
-      handle: repo.handle,
-      // FIXME: We assume the account is active, because Tap doesn't tell us:
-      status: 'active',
-      isActive: true,
-    })
-  }
-
-  if (evt.collection === lexicon.fyi.questionable.actor.profile.$nsid) {
-    if (evt.rkey !== 'self') {
-      return
-    }
-
-    if (evt.action === 'delete') {
-      await Profile.query().where('did', evt.did).delete()
-      return
-    }
-
-    const profile = lexicon.fyi.questionable.actor.profile.$safeParse(evt.record)
-    if (!profile.success) {
-      logger.info(
-        { record: evt.record },
-        `Invalid ${lexicon.fyi.questionable.actor.profile.$nsid} record for: ${evt.did}`
-      )
-      return
-    }
-
-    logger.debug(profile.value, `Parsed profile for ${evt.did}`)
-
-    await Profile.upsert(evt.did, profile.value, DateTime.now())
-  }
+  await Profile.query().where('did', evt.did).delete()
 })
+
+// Handle both creates and updates of actor profiles:
+indexer.put(lexicon.fyi.questionable.actor.profile, async (evt) => {
+  if (evt.rkey !== 'self') {
+    return
+  }
+
+  logger.debug(evt.record, `Updating profile for ${evt.did}`)
+
+  await Profile.upsert(evt.did, evt.record, DateTime.now())
+})
+
+indexer.error((err) => {
+  logger.error(err, 'Error processing Tap event')
+})
+
+// Set the indexer to use with Tap:
+tap.setIndexer(indexer)
+
+if (app.getEnvironment() === 'web' && app.inDev) {
+  tap.startIndexer()
+}
