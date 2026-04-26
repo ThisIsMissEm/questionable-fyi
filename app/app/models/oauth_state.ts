7eefa60095e84bf2c4f43e9f29d0c08c629b6b2f
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2026-04-26 21:04:34 +0200

    New models

--- app/app/models/oauth_state.ts
+++ app/app/models/oauth_state.ts
@@ -1,13 +1,3 @@
-import { BaseModel, column } from '@adonisjs/lucid/orm'
-import { DateTime } from 'luxon'
+import { OauthStateSchema } from '#database/schema'
 
-export default class OauthState extends BaseModel {
-  @column({ isPrimary: true })
-  declare key: string
-
-  @column()
-  declare value: string
-
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
-  declare updatedAt: DateTime
-}
+export default class OauthState extends OauthStateSchema {}
